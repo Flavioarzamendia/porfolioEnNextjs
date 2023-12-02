ca09b514d01910dd5aee89cf94b4142d3a14b03e
Flavio Arzamendia <flavioarzamendia2019@gmail.com>
2023-12-02 16:01:38 -0300

    Arreglo de titulo

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ import { Analytics } from '@vercel/analytics/react';
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'FA Desarrollo Web|Marketing Digital',
+  title: 'FA Desarrollo Web | Marketing Digital',
   description: 'Soy un desarrollador web apasionado con experiencia en WordPress, HTML5, CSS3 y JavaScript. Ofrezco servicios de diseño web personalizado.'
 }
 
