892d07ecc5dae961dcb0174b0ee7799813c38dc4
Flavio Arzamendia <flavioarzamendia2019@gmail.com>
2024-01-08 19:55:16 -0300

    arreglo de script de año

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <div className="container p-12 flex justify-between ">
         <span className=" text-white sm:px-4 ml-4">FA</span>
         <h4 className="text-white px-4">Convierte tu visión en realidad.</h4>
-        <p className="text-white"> © Flavio Arzamendia {today.getFullYear()}</p>
+        <p className="text-white"> © Flavio Arzamendia {new Date().getFullYear()}</p>
       </div>
     </footer>
   );
